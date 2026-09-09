68ea5c2fad669858793681ea19996fa63a88130d
Ryan Fihr Hulton <Ryanf.hulton@gmail.com>
2026-09-09 10:32:00 +0200

    Add - read_article.py

---
+++ backend/read_article.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+def ask_and_read_article() -> str:
+    """Demande un article jusqu'à ce qu'un fichier existant soit choisi."""
+
+    articles_folder = Path(__file__).parent.parent / "articles"
+
+    while True:
+        filename = input("Nom de l'article : ")
+
+        try:
+            article_path = articles_folder / f"{filename}.md"
+            content = article_path.read_text(encoding="utf-8")
+            return content
+
+        except FileNotFoundError:
+            print("Cet article n'existe pas. Réessaie.")
+
+
+print(ask_and_read_article())
